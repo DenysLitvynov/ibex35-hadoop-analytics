66d6d7b048393c99ac60c5fb4526e7b9c7da44d2
Denys Litvynov <denislit741@gmail.com>
2026-02-23 08:51:53 +0100

    Eliminar archivos de práctica que no pertenecen al proyecto

--- ej16.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while read street; do
-    for year in 2017 2018 2019 2020; do
-        count=$(hdfs dfs -cat crimes/$year/*.csv 2>/dev/null | grep -i "$street" | wc -l)
-        echo "Calle $street - Año $year: $count"
-    done
-done < /tmp/streets.txt
