bb53b8f5506ae05d68f7ab97405f6d7f5d982b27
Denys Litvynov <denislit741@gmail.com>
2026-03-18 14:11:07 +0100

    Analisis de relación entre el ROE y la revalorización anual implementado

--- src/avanzado2_roe_rendimiento.py
+++ src/avanzado2_roe_rendimiento.py
@@ -16,7 +16,13 @@ class ROERendimiento(MRJob):
         if len(filas[1]) == 10 and filas[1][4] == '-':
             yield filas[0], ('cotizaciones',filas[1], float(filas[2]))
         else:
-            yield filas[0], ('fundamentales', float(filas[3]))
+            try: 
+                roe_str = filas[3]
+                if roe_str != "":
+                    roe = float(roe_str)
+                    yield filas[0], ('fundamentales', roe)
+            except (ValueError, IndexError):
+                pass
 
     """ Ordenamos los registros de cotizaciones por fecha, 
     obtenemos el primer valor del año y el ultimo valor del año,
@@ -27,17 +33,23 @@ class ROERendimiento(MRJob):
         for value in values:
             tipo = value[0]
             if tipo == 'cotizaciones':
-                precios.append(value[2])
+                precios.append((value[1], value[2]))
             elif tipo == 'fundamentales':
-                # Convertimos el ROE a porcentaje
                 roe = round(value[1] * 100, 2)
-        if roe > 0 and roe is not None and len(precios) > 0:
-            precio_inicial = precios[0]
-            precio_final = precios[-1]
-            porcentaje_revalorizacion = round(((precio_final - precio_inicial) / precio_inicial) * 100, 2)
-            yield key, ('ROE: ' + str(roe) + '%', 'Porcentaje de revalorizacion: ' + str(porcentaje_revalorizacion) + '%')
-        else:
-            yield key, ('ROE: ' + str(roe) + '%', 'Porcentaje de revalorizacion: 0%')
+ 
+        # Solo emitimos si tenemos ROE válido y precios
+        if roe is None or roe == 0 or len(precios) == 0:
+            return
+ 
+        precios_ordenados = sorted(precios, key=lambda x: x[0])
+        precio_inicial = precios_ordenados[0][1]
+        precio_final = precios_ordenados[-1][1]
+ 
+        if precio_inicial <= 0:
+            return
+ 
+        porcentaje_revalorizacion = round(((precio_final - precio_inicial) / precio_inicial) * 100, 2)
+        yield key, ('ROE: ' + str(roe) + '%', 'Revalorizacion: ' + str(porcentaje_revalorizacion) + '%')
 
 
 if __name__ == '__main__':
